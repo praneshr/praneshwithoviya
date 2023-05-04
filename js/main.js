4a8141c1ebc10253de2ea9fb2213d2ac5b6db30e
Pranesh Ravi <praneshravi@Praneshs-Mac-mini.local>
2023-05-03 22:40:05 -0400

    Updating details

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
+    $(document.body).removeClass('hide');
+    
     $('.slick').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         centerMode: true,
+        responsive: [
+            {
+                breakpoint: 995,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $('.button').click(function () {
